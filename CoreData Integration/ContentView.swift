f409d34c12e09daa4bca7dd1419782e25abb4908
R4S3C <r4s3c@icloud.com>
2019-09-21 10:34:09 -0600

    Initial Commit

---
+++ CoreData Integration/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoreData Integration
+//
+//  Created by R4S3C on 2019-09-21.
+//  Copyright © 2019 R4S3C. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CoreData IntegrationTests/CoreData_IntegrationTests.swift
@@ -0,0 +1,34 @@
+//
+//  CoreData_IntegrationTests.swift
+//  CoreData IntegrationTests
+//
+//  Created by R4S3C on 2019-09-21.
+//  Copyright © 2019 R4S3C. All rights reserved.
+//
+
+import XCTest
+@testable import CoreData_Integration
+
+class CoreData_IntegrationTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
